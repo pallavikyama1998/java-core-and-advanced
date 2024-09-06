8c0228604260da77d4b9fe5a87b4851520ee53c3
pallavikyama1998 <pallavikyama1998@gmail.com>
2024-09-05 23:58:55 -0700

    LCM and GCD to be continued

--- In28Minutes/eclipse-workspace/java-for-beginners/java-coding-exercises/src/com/in28minutes/coding/exercises/set4/q22/BiNumberRunner.java
+++ In28Minutes/eclipse-workspace/java-for-beginners/java-coding-exercises/src/com/in28minutes/coding/exercises/set4/q22/BiNumberRunner.java
@@ -5,30 +5,39 @@ public class BiNumberRunner {
 	public static void main(String[] args) {
 		BiNumber binum = new BiNumber(10, -1);
 		System.out.println(binum.calculateLCM()); // Output: -1
+		System.out.println(binum.calculateLCMUsingForLoop());
 
 		binum = new BiNumber(10, 10);
 		System.out.println(binum.calculateLCM()); // Output: 10
+		System.out.println(binum.calculateLCMUsingForLoop());
 
 		binum = new BiNumber(15, 20);
 		System.out.println(binum.calculateLCM()); // Output: 60
+		System.out.println(binum.calculateLCMUsingForLoop());
 
 		binum = new BiNumber(-1, -1);
 		System.out.println(binum.calculateLCM()); // Output: -1
+		System.out.println(binum.calculateLCMUsingForLoop());
 
 		binum = new BiNumber(10, 0);
 		System.out.println(binum.calculateLCM()); // Output: 0
+		System.out.println(binum.calculateLCMUsingForLoop());
 
 		binum = new BiNumber(-15, 20);
 		System.out.println(binum.calculateLCM()); // Output: -1
+		System.out.println(binum.calculateLCMUsingForLoop());
 
 		binum = new BiNumber(0, 20);
 		System.out.println(binum.calculateLCM()); // Output: 0
+		System.out.println(binum.calculateLCMUsingForLoop());
 
 		binum = new BiNumber(0, 0);
 		System.out.println(binum.calculateLCM()); // Output: 0
+		System.out.println(binum.calculateLCMUsingForLoop());
 
 		binum = new BiNumber(9, 6);
 		System.out.println(binum.calculateLCM()); // Output: 18
+		System.out.println(binum.calculateLCMUsingForLoop());
 	}
 
 }
